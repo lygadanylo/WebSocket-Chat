f85fd04716487653105e7225636dfbd69051d091
Danylo Lyha <danylo.lyha@synapse.com>
2020-01-22 17:29:07 +0200

    return jsx

--- client/src/components/account.js
+++ client/src/components/account.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Account extends Component {
 	render() {
-		return <div className="account-info-wrapper col-xl-6 col-lg-6 col-md-6 col-sm-6">asd</div>;
+		return <div className="account-info-wrapper col-sm-6 col-md-6 col-lg-6 ol-xl-6">asd</div>;
 	}
 }
 
--- client/src/components/chatWindow.js
+++ client/src/components/chatWindow.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ChatWindow extends Component {
 	render() {
-		return <div className="chat-window-wrapper" />;
+		return <div className="chat-window-wrapper col-sm-6 col-md-6 col-lg-6 ol-xl-6">zxc</div>;
 	}
 }
 
